02cc1580dae21ab39625caaaa716577677422073
qouteall <qouteall@163.com>
2020-07-22 17:12:26 +0800

    ...

---
+++ src/main/java/com/qouteall/immersive_portals/mixin/altius_world/MixinChunkStatus.java
@@ -0,0 +1,121 @@
+package com.qouteall.immersive_portals.mixin.altius_world;
+
+import com.qouteall.immersive_portals.Helper;
+import com.qouteall.immersive_portals.altius_world.AltiusInfo;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.WorldGenRegion;
+import net.minecraft.world.gen.feature.structure.StructureManager;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+@Mixin(ChunkStatus.class)
+public class MixinChunkStatus {
+    //vanilla feature generation is not thread safe
+    
+    private static ReentrantLock featureGenLock;
+    
+    @Redirect(
+        method = "*",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/gen/ChunkGenerator;func_230351_a_(Lnet/minecraft/world/gen/WorldGenRegion;Lnet/minecraft/world/gen/feature/structure/StructureManager;)V"
+        )
+    )
+    private static void redirectGenerateFeatures(
+       ChunkGenerator chunkGenerator, WorldGenRegion region, StructureManager accessor
+    ) {
+        boolean shouldLock = getShouldLock();
+        if (shouldLock) {
+            featureGenLock.lock();
+        }
+        try {
+            chunkGenerator.func_230351_a_(region,accessor);
+        }
+        catch (Throwable e) {
+            Helper.err(String.format(
+                "Error when generating terrain %s %d %d",
+                region.getWorld().func_234923_W_(),
+                region.getMainChunkX(),
+                region.getMainChunkZ()
+            ));
+            e.printStackTrace();
+        }
+        if (shouldLock) {
+            featureGenLock.unlock();
+        }
+    }
+    
+    @Redirect(
+        method = "*",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/gen/ChunkGenerator;func_235954_a_(Lnet/minecraft/world/gen/feature/structure/StructureManager;Lnet/minecraft/world/chunk/IChunk;Lnet/minecraft/world/gen/feature/template/TemplateManager;J)V"
+        )
+    )
+    private static void redirectSetStructureStarts(
+        ChunkGenerator generator,
+        StructureManager structureAccessor, IChunk chunk, TemplateManager structureManager, long l
+    ) {
+        boolean shouldLock = getShouldLock();
+        if (shouldLock) {
+            featureGenLock.lock();
+        }
+        try {
+            generator.func_235954_a_(structureAccessor, chunk, structureManager, l);
+        }
+        catch (Throwable e) {
+            Helper.err(String.format(
+                "Error when generating terrain %s",
+                chunk
+            ));
+            e.printStackTrace();
+        }
+        if (shouldLock) {
+            featureGenLock.unlock();
+        }
+    }
+    
+    @Redirect(
+        method = "*",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/gen/ChunkGenerator;func_235953_a_(Lnet/minecraft/world/IWorld;Lnet/minecraft/world/gen/feature/structure/StructureManager;Lnet/minecraft/world/chunk/IChunk;)V"
+        )
+    )
+    private static void redirectAddStructureReference(
+        ChunkGenerator chunkGenerator, IWorld world, StructureManager structureAccessor, IChunk chunk
+    ) {
+        boolean shouldLock = getShouldLock();
+        if (shouldLock) {
+            featureGenLock.lock();
+        }
+        try {
+            chunkGenerator.func_235953_a_(world, structureAccessor, chunk);
+        }
+        catch (Throwable e) {
+            Helper.err(String.format(
+                "Error when generating terrain %s",
+                chunk
+            ));
+            e.printStackTrace();
+        }
+        if (shouldLock) {
+            featureGenLock.unlock();
+        }
+    }
+    
+    private static boolean getShouldLock() {
+        return AltiusInfo.isAltius();
+    }
+    
+    static {
+        featureGenLock = new ReentrantLock(true);
+    }
+}
