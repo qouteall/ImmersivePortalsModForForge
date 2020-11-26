83bf34cbb9a38e255564c28d2b2afc837a4d8412
Dylan Jones <dylanjones2011@gmail.com>
2020-11-26 02:14:32 -0500

    Fix a bug related to attributes on players.
    This mixin is used when running under Fabric to add a custom attribute
    to players to tweak the reach distance.  However, it isn't needed under
    forge and causes an issue where other mods' custom attributes don't get
    added to new players properly.  I just added a check so that it doesn't
    run under Forge which seems to fix the issue, at least with Botania.

--- src/main/java/com/qouteall/immersive_portals/mixin/common/block_manipulation/MixinPlayerEntity_B.java
+++ src/main/java/com/qouteall/immersive_portals/mixin/common/block_manipulation/MixinPlayerEntity_B.java
@@ -1,5 +1,6 @@
 package com.qouteall.immersive_portals.mixin.common.block_manipulation;
 
+import com.qouteall.hiding_in_the_bushes.O_O;
 import com.qouteall.immersive_portals.block_manipulation.HandReachTweak;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,14 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public class MixinPlayerEntity_B {
     @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;func_234570_el_()Lnet/minecraft/entity/ai/attributes/AttributeModifierMap$MutableAttribute;", at = @At("RETURN"), cancellable = true)
-    private static void onCreatePlayerAttributes(
-        CallbackInfoReturnable<AttributeModifierMap.MutableAttribute> cir
-    ) {
-        cir.setReturnValue(
-            cir.getReturnValue().func_233815_a_(
-                HandReachTweak.handReachMultiplierAttribute,
-                1.0
-            )
-        );
+    private static void onCreatePlayerAttributes(CallbackInfoReturnable<AttributeModifierMap.MutableAttribute> cir) {
+        // We only need to use this attribute on Fabric, not Forge.  If we inject on forge it causes
+        // compatibility problems with other mods.
+        if (!O_O.isForge()) {
+            cir.setReturnValue(cir.getReturnValue().func_233815_a_(HandReachTweak.handReachMultiplierAttribute, 1.0));
+        }
     }
 }
