e856d98753d3647eea5913adc271aeb2d1756c46
qouteall <qouteall@163.com>
2021-02-09 18:42:49 +0800

    fix particle on forge

---
+++ src/main/java/com/qouteall/hiding_in_the_bushes/mixin/client/MixinParticleManager_Forge.java
@@ -0,0 +1,25 @@
+package com.qouteall.hiding_in_the_bushes.mixin.client;
+
+import com.qouteall.immersive_portals.render.context_management.RenderStates;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ParticleManager.class)
+public class MixinParticleManager_Forge {
+    @Redirect(
+        method = "renderParticles(Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer$Impl;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/renderer/ActiveRenderInfo;FLnet/minecraft/client/renderer/culling/ClippingHelper;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/particle/Particle;shouldCull()Z"
+        )
+    )
+    private boolean redirectShouldCull(Particle particle) {
+        if (!RenderStates.shouldRenderParticle(particle)) {
+            return true;
+        }
+        return particle.shouldCull();
+    }
+}
