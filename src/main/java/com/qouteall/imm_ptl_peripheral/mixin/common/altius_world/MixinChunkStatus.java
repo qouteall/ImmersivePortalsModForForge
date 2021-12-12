7feb22297c9d48a52ba85fe65a948967af5e32ed
qouteall <qouteall@163.com>
2021-12-12 16:32:34 +0800

    fix crash with new version of mixin

--- src/main/java/com/qouteall/imm_ptl_peripheral/mixin/common/altius_world/MixinChunkStatus.java
+++ src/main/java/com/qouteall/imm_ptl_peripheral/mixin/common/altius_world/MixinChunkStatus.java
@@ -4,29 +4,31 @@ import com.qouteall.imm_ptl_peripheral.altius_world.AltiusInfo;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.WorldGenRegion;
 import net.minecraft.world.server.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
 @Mixin(ChunkStatus.class)
 public class MixinChunkStatus {
-    @Inject(
+    @Redirect(
         method = "*",
         at = @At(
             value = "INVOKE",
-            shift = At.Shift.AFTER,
             target = "Lnet/minecraft/world/gen/ChunkGenerator;generateSurface(Lnet/minecraft/world/gen/WorldGenRegion;Lnet/minecraft/world/chunk/IChunk;)V"
         )
     )
-    private static void redirectBuildSurface(
-        ServerWorld world, ChunkGenerator generator, List<IChunk> surroundingChunks, IChunk chunk,
-        CallbackInfo ci
+    private static void redirectGenerateSurface(
+        ChunkGenerator chunkGenerator,
+        WorldGenRegion region, IChunk chunk
     ) {
-        AltiusInfo.replaceBedrock(world, chunk);
+        chunkGenerator.generateSurface(region, chunk);
+        AltiusInfo.replaceBedrock(region.getWorld(), chunk);
     }
     
 }
